8eb5e417e0f9e6d79a03e2d4afa395f89f92cd65
Ayush Kumar Jha B <rookie@Ayushs-MacBook-Air.local>
2026-02-04 23:30:57 +0530

    added get food endpoint

--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -6,10 +6,11 @@ const multer=require("multer");
 
 const upload=multer({
     storage:multer.memoryStorage(), //stores the file buffer temporaly on the servers ram and and hard disk 
-    
 })
 
 //POST method /api/food/ {Protected} //endpoint using which the foodpartner can post the food item to the backend
-router.post("/",authMiddleware.authFoodPartnerMiddleware,)
+router.post("/",authMiddleware.authFoodPartnerMiddleware, upload.single("video"),foodController.createFood)
+
+router.get("/",authMiddleware.authUserMiddleware,foodController.getFoodItems);
 
 module.exports=router;
