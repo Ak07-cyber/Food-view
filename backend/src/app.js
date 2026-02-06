96dc52c3d1666f30fdbc89d07badaa757d81df38
Ayush Kumar Jha B <rookie@Ayushs-MacBook-Air.local>
2026-02-06 10:53:05 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,14 @@
 const express=require("express");
 const cookieParser=require('cookie-parser');
 const authRoutes=require('./routes/auth.routes');
+const foodRoutes=require("./routes/food.routes")
+const cors =require("cors");
 
 const app=express();
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 //parsing the body json using the middleware
 app.use(express.json());
